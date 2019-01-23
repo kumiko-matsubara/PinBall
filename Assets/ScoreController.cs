118e17ab26937a35c60498f31aa476b3eef3b10f
kumiko1203 <qoo@mrsk.co.jp>
2019-01-23 10:44:47 +0900

    PinBall初めてのコミット

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+    //Scoreを表示するテキスト
+    private 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
